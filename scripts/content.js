acd8582897e0e62d5ce04ee85ce9afbc58405b8a
calebtham <calebtham02@gmail.com>
2023-08-04 22:28:36 +0100

    Formatting

--- scripts/content.js
+++ scripts/content.js
@@ -1,27 +1,27 @@
 const waitForElementByQuery = (selector) => {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
 
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
-            }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
     });
-}
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+};
 
 const observeUrlChange = () => {
   let oldHref = document.location.href;
   const body = document.querySelector("body");
-  const observer = new MutationObserver(mutations => {
+  const observer = new MutationObserver(() => {
     if (oldHref !== document.location.href) {
       oldHref = document.location.href;
       main();
@@ -31,9 +31,11 @@ const observeUrlChange = () => {
 };
 
 const main = () => {
-    waitForElementByQuery("#movie_player > div.ytp-player-content.ytp-iv-player-content > div")
-        .then(profilePic => profilePic.remove())
+  waitForElementByQuery(
+    "#movie_player > div.ytp-player-content.ytp-iv-player-content > div"
+  ).then((profilePic) => profilePic.remove());
 };
 
 window.onload = observeUrlChange;
+
 main();
